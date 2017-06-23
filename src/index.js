54e0f09309120e0381941809e4da77bee8d99b71
Amogh Banta <amoghbanta@gmail.com>
2017-06-23 12:55:10 +0530

    add: component for Linear Gradient :heavy_plus_sign:

--- src/index.js
+++ src/index.js
@@ -14,10 +14,12 @@ import RecyclerView from './RecyclerView';
 import Spinner from './Spinner';
 import TextArea from './TextArea';
 import TextInput from './TextInput';
+import Carousel from './Carousel';
+import LinearGradient from './LinearGradient';
 
 module.exports = {
     AutoComplete, Button, CheckBox, Dropdown,
     IconButton, Image, Label, MultiSelect,
     MultiSelectItem, ProgressBar, RadioGroup, RadioButton,
-    RecyclerView, Spinner, TextArea, TextInput
+    RecyclerView, Spinner, TextArea, TextInput, Carousel, LinearGradient
  };
